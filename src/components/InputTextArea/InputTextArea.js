8a5ce8be8c19632950c16b85a28c831afac5ccb8
Naysha Santiago <49569570+nayshasantiago@users.noreply.github.com>
2022-11-03 14:19:57 -0500

    edit component

--- src/components/InputTextArea/InputTextArea.js
+++ src/components/InputTextArea/InputTextArea.js
@@ -4,7 +4,13 @@ const InputTextArea = (props) => {
             <label htmlFor={ props.id } className="leading-7 text-sm text-gray-600">
                 { props.label }
             </label>
-            <textarea type={ props.type } name={ props.id } value={ props.value } onChange={ props.onChange } className="w-full h-36 bg-white resize-none rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+            <textarea 
+                type={ props.type } 
+                name={ props.id } 
+                data-testid="input-text-area"
+                value={ props.value } 
+                onChange={ props.onChange } 
+                className="w-full h-36 bg-white resize-none rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
             <span className="mt-1 text-rose-600 text-xs italic">
                 { props.errorMsg }
             </span>
