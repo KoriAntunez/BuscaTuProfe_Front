27e07a6aba8e2beeca04eded9ed9fc57d8e27e73
KoriAntunez <kori.antunez@unmsm.edu.pe>
2022-12-22 01:45:26 -0500

    commit

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,12 +1,14 @@
 const Logo = (props) => {
-    return (
-        <div className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-            <img className="w-10" src={ require("../assets/img/logo_icono.png") } alt="logo"/>
-            <span className={ `ml-3 ${ props.style_span }` }>
-                BuscaTuProfe
-            </span>
-        </div>
-    );
-}
+  return (
+    <div className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+      <img
+        className="w-10"
+        src={require("../../assets/img/logo_icono.png")}
+        alt="logo"
+      />
+      <span className={`ml-3 ${props.style_span}`}>BuscaTuProfe</span>
+    </div>
+  );
+};
 
 export default Logo;
